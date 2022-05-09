3f6c49c5ebbc76dcb2d237b70e8bf7ecebb7a08f
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-05-09 14:53:45 +0530

    Update printOfNumbers.js

--- printOfNumbers.js
+++ printOfNumbers.js
@@ -11,3 +11,17 @@ function printNumber(from, to){
 }
 
 printNumber(1, 10)
+
+// using Nested setTimeout
+
+function printNumbers(begin, end){
+    let a = begin;
+    setTimeout(function go() {
+    alert (a)
+        if(current < to ) {
+        setTimeout(go, 1000)
+        }
+        a++;
+    }, 1000);
+}
+printNumbers(1, 20)
